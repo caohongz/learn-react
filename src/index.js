81ea72d9639f8ed3866a63af7393adde3ad50b2a
caohongz <18511283565@163.com>
2019-04-22 16:57:26 -0400

    增加控制面板来显示三个计数器

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
 /*
  * @Author: Cao.Hongzhi 
  * @Date: 2019-04-22 16:15:26 
- * @Last Modified by:   Cao.Hongzhi 
- * @Last Modified time: 2019-04-22 16:15:26 
+ * @Last Modified by: Cao.Hongzhi
+ * @Last Modified time: 2019-04-22 16:46:34
  */
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ControlPanel from './ControlPanel';
 import './index.css';
-import ClickCounter from './ClickCounter';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<ClickCounter />, document.getElementById('root'));
+ReactDOM.render(
+  <ControlPanel/>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
