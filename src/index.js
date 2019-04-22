38b23406fbe02b03db5b8b27eaeb322dc9eb36aa
caohongz <18511283565@163.com>
2019-04-22 16:21:14 -0400

    第一次提交，添加点击计数组件

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
+/*
+ * @Author: Cao.Hongzhi 
+ * @Date: 2019-04-22 16:15:26 
+ * @Last Modified by:   Cao.Hongzhi 
+ * @Last Modified time: 2019-04-22 16:15:26 
+ */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import ClickCounter from './ClickCounter';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<ClickCounter />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
